b78c172ded58a5266dc9f0b82b7aae8b8130f449
mk <username@users.noreply.github.com>
2025-04-28 10:58:56 +0800

    fix: console log clean up

--- src/react-agent/utils.ts
+++ src/react-agent/utils.ts
@@ -5,19 +5,6 @@ import { initChatModel } from "langchain/chat_models/universal";
  * @param fullySpecifiedName - String in the format 'provider/model' or 'provider/account/provider/model'.
  * @returns A Promise that resolves to a BaseChatModel instance.
  */
-// export async function loadChatModel(
-//   fullySpecifiedName: string,
-// ): Promise<ReturnType<typeof initChatModel>> {
-//   const index = fullySpecifiedName.indexOf("/");
-//   if (index === -1) {
-//     // If there's no "/", assume it's just the model
-//     return await initChatModel(fullySpecifiedName);
-//   } else { 
-//     const provider = fullySpecifiedName.slice(0, index);
-//     const model = fullySpecifiedName.slice(index + 1);
-//     return await initChatModel(model, { modelProvider: provider });
-//   }
-// }
 
 export async function loadChatModel(
   fullySpecifiedName: string,
