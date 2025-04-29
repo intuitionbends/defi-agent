3c775a8f950100610fa72fc181c5e653114ac867
jellybean <jellybean@delaunch.ai>
2025-04-29 19:52:49 +0900

    feat: add service (#11)

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,6 +1,7 @@
 import express, { Router, Request, Response, NextFunction } from "express";
 import { DatabaseService } from "../core/services/Database";
 import { Chain } from "../types/enums";
+import { RiskTolerance } from "../types/types";
 
 export function createApiV1Router(dbService: DatabaseService): Router {
   const router = express.Router();
@@ -18,10 +19,23 @@ export function createApiV1Router(dbService: DatabaseService): Router {
 
   router.get("/pool_yields/suggest", async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const { riskTolerance, maxDrawdown, expectedAPR, capitalSize, investmentTimeframe } =
-        req.query;
+      // TODO: update default values
+      const {
+        riskTolerance = RiskTolerance.Low,
+        maxDrawdown = "0.2",
+        asset = "USDT",
+        assetValueUsd = "1000",
+        investmentTimeframe = "30",
+      } = req.query;
 
-      const yields = await dbService.getTopAPYPoolYields(Chain.Aptos, 100_000, 3);
+      const yields = await dbService.getQualifiedPoolYields(
+        Chain.Aptos,
+        riskTolerance as RiskTolerance,
+        Number(maxDrawdown),
+        asset as string,
+        Number(assetValueUsd),
+        Number(investmentTimeframe),
+      );
 
       res.json(yields);
     } catch (error) {
